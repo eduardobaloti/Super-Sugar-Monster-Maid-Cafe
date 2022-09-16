855bc25c31b532e6ffe463540e0bbd3429f31b16
eduardobaloti <g.eduardobaloti@gmail.com>
2022-09-15 22:58:30 -0300

    initial

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public Animator animator;
+
+    public int maxHealth = 1000;
+    public int currentHealth;
+    Vector2 move;
+    Vector2 maidPosition;
+
+    public Transform maid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;  
+
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move = new Vector2(0.005f, 0.005f) * Time.deltaTime;
+        //maidPosition = maid.GetComponent<Transform>().position;
+
+
+       // rb.transform.Translate(rb.position += move);
+        
+    }
+
+    private void OnCollisionEnter2D(Collision collision) {
+        /*
+        if (collision.gameObject.name == "wall")
+        {
+            move = new Vector2(-0.005f, 0f);
+            print("colid");
+        }
+
+         if (collision.gameObject.name == "wall")
+        {
+            move = new Vector2(-0.005f, 0f);
+            print("colid");
+        }
+        */
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        animator.SetTrigger("damage");
+
+
+        if(currentHealth <= 0)
+        {
+            //WaitForSecondsRealtime(1);
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            this.enabled = false;
+        }
+        //damage
+    }
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System.Data;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public AudioSource slash; 
+
+    Rigidbody2D rb;
+    public float speed = 10f;
+    bool walking = false;
+
+
+    //Attack methods
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+
+    public Animator animator;
+
+
+    enum pos{
+        top,
+        bottom,
+        left,
+        right,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Physics.IgnoreLayerCollision(5,6);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        rb.MovePosition(transform.position + (move * Time.deltaTime * speed));   
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+    }
+    
+    void Attack()
+    {
+        //slash.PlayOneShot(slash.clip, 1);
+
+        
+        animator.SetTrigger("attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Hited");
+
+            enemy.GetComponent<Monster>().TakeDamage(1);
+            
+
+            
+        }
+
+
+    }
+
+    void OnDrawGizmosSelected() {
+
+        if (attackPoint.position == null) return;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+/*
+    void OnCollisionEnter(Collision other) 
+    {
+        //MonsterColision    
+    }
+
+*/
+}
+                       
