0f740fafbbbf121edb1c51d5a9a0055b6f8f52e8
eduardobaloti <g.eduardobaloti@gmail.com>
2023-01-25 22:34:01 -0300

    art adjustment

--- Assets/Scripts/GenericMonster.cs
+++ Assets/Scripts/GenericMonster.cs
@@ -9,12 +9,15 @@ public class GenericMonster : MonoBehaviour
     public Animator animator;
     public AudioSource source;
     public AudioClip beated;
+    float old_pos;
 
     //Monster stats
-    int maxHealth = 3;
+    public int maxHealth = 3;
     int currentHealth;
     public float speed = 1;
     bool isHitted = false;
+    public string Mtype = monster;
+
 
 
     public Transform maid;
@@ -22,9 +25,11 @@ public class GenericMonster : MonoBehaviour
 
     void Start()
     {
-        Physics.IgnoreLayerCollision(6 ,8); //items
+        Physics.IgnoreLayerCollision(6, 8); //items
         currentHealth = maxHealth;
         rb = GetComponent<Rigidbody2D>();
+
+        old_pos = transform.position.x;
     }
 
     void FixedUpdate()
@@ -33,6 +38,17 @@ public class GenericMonster : MonoBehaviour
 
         rb.MovePosition(transform.position + ((maid.position * 1f * Time.deltaTime)) * speed);
 
+        if (old_pos < transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            print("teste 2");
+        }
+
+        if (old_pos > transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            print("teste 3");
+        }
 
 
         //PatrolWait();                  
@@ -70,7 +86,7 @@ public class GenericMonster : MonoBehaviour
     IEnumerator Hurted()
     {
         var oldSpeed = speed;
-        speed = speed * 0.1f; 
+        speed = speed * 0.1f;
         isHitted = true;
         yield return new WaitForSeconds(0.25f);
         isHitted = false;
